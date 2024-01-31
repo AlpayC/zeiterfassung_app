265041a3c720cde27c011e40c674f3b364ee5c5a
AlpayC <alpaycelik91@googlemail.com>
2024-01-31 17:51:28 +0100

    added helper func to calculate time Difference

--- frontend/src/utils/formatDate.jsx
+++ frontend/src/utils/formatDate.jsx
@@ -5,7 +5,8 @@ export const formatDate = (date) => {
 export const formatTime = (date) => {
   const hours = date.getHours().toString().padStart(2, "0");
   const minutes = date.getMinutes().toString().padStart(2, "0");
-  return `${hours}:${minutes}`;
+  const seconds = date.getSeconds().toString().padStart(2, "0");
+  return `${hours}:${minutes}:${seconds}`;
 };
 export const convertUtcToGermanTime = (date) => {
   const utcDateTime = new Date(date);
@@ -13,4 +14,13 @@ export const convertUtcToGermanTime = (date) => {
   germanDateTime.setHours(germanDateTime.getHours() + 1);
 
   return germanDateTime.toISOString();
+};
+export const calculateTimeDifferenceInSeconds = (time1, time2) => {
+  const [hours1, minutes1, seconds1] = time1.split(":").map(Number);
+  const [hours2, minutes2, seconds2] = time2.split(":").map(Number);
+
+  const totalSeconds1 = hours1 * 3600 + minutes1 * 60 + seconds1;
+  const totalSeconds2 = hours2 * 3600 + minutes2 * 60 + seconds2;
+
+  return Math.abs(totalSeconds1 - totalSeconds2);
 };
