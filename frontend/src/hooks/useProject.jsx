4341a8c5a485d2131f327855eaf56d0b895623ea
AlpayC <alpaycelik91@googlemail.com>
2024-02-20 19:52:40 +0100

    wip: updated useProjectHook to updateprojectStatus

--- frontend/src/hooks/useProject.jsx
+++ frontend/src/hooks/useProject.jsx
@@ -17,8 +17,10 @@ export default function useProject({
   const { refetchProjects } = useContext(ProjectsContext);
 
   const updateProject = async (event) => {
-    event.preventDefault();
-    console.log(event.target.value);
+    if (event) {
+      event.preventDefault();
+      console.log(event.target.value);
+    }
     const requestData = {
       title: projectTitle,
       email: user.email,
@@ -52,5 +54,71 @@ export default function useProject({
       );
     }
   };
-  return { updateProject };
+  const updateProjectStatus = async (projectStatus, newActiveStatus) => {
+    const projectStatuses = await projectStatus.map((status) => ({
+      ...status,
+      status: status.label === newActiveStatus.label,
+    }));
+
+    const requestData = {
+      email: user.email,
+      projectId: id,
+      projectStatus: projectStatuses,
+    };
+
+    try {
+      const response = await axios.put(
+        `/api/projectmanagement/updateProject/${id}`,
+        requestData
+      );
+      showAlert(
+        `${response?.data.message}`,
+        `${response?.data.success.message}`,
+        "alert-success",
+        3000
+      );
+      refetchProjects();
+      console.log(response);
+    } catch (e) {
+      console.error("Error:", e);
+      showAlert(
+        `${e.response?.data.message}`,
+        ` ${e.response?.data.error.message}`,
+        "alert-error",
+        4000
+      );
+    }
+  };
+  const updateProjectTags = async (tags) => {
+    const requestData = {
+      email: user.email,
+      projectId: id,
+      tags: tags,
+    };
+
+    try {
+      const response = await axios.put(
+        `/api/projectmanagement/updateProject/${id}`,
+        requestData
+      );
+      showAlert(
+        `${response?.data.message}`,
+        `${response?.data.success.message}`,
+        "alert-success",
+        3000
+      );
+      refetchProjects();
+      console.log(response);
+    } catch (e) {
+      console.error("Error:", e);
+      showAlert(
+        `${e.response?.data.message}`,
+        ` ${e.response?.data.error.message}`,
+        "alert-error",
+        4000
+      );
+    }
+  };
+
+  return { updateProject, updateProjectStatus, updateProjectTags };
 }
